de2f37c4d98e0042a4d3e87f6da95e70548d825b
Andrew Willey <andwilley@gmail.com>
2018-06-17 06:54:30 -0700

    Update index.d.ts with full callback signature.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
 declare interface Array<T> {
-    filterMap(filterCallback: (element: any) => boolean, mapCallback: (element: any) => Array<any>): Array<any>; 
+    filterMap(filterCallback: (element: any, index?: number, origArray?: any[]) => boolean,
+              mapCallback: (element: any, index?: number, origArray?: any[]) => Array<any>
+    ): Array<any>; 
 }
