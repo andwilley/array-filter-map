5f040ea22c67596dce06fcaa29bbc3a4d9bd297e
Andrew Willey <andwilley@gmail.com>
2018-06-17 08:32:23 -0700

    Update index.d.ts with optional thisArg.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 declare interface Array<T> {
     filterMap(filterCallback: (element: any, index?: number, origArray?: any[]) => boolean,
-              mapCallback: (element: any, index?: number, origArray?: any[]) => Array<any>
-    ): Array<any>; 
+              mapCallback: (element: any, index?: number, origArray?: any[]) => Array<any>,
+              thisArg?: any
+    ): Array<any>;
 }
